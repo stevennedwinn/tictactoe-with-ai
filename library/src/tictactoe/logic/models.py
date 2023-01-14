3db66e35d796a1874bd3782848add7d825f2a910
stevennedwinn <108059776+stevennedwinn@users.noreply.github.com>
2023-01-14 22:58:31 +0800

    [UPDATE] new class Grid and its count properties

--- library/src/tictactoe/logic/models.py
+++ library/src/tictactoe/logic/models.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
+from dataclasses import dataclass
+from functools import cached_property
 
 import enum
+import re
 
 class Mark(str, enum.Enum):
     CROSS = "X"
@@ -9,3 +12,23 @@ class Mark(str, enum.Enum):
     @property
     def other(self) -> Mark:
         return Mark.CROSS if self is Mark.NAUGHT else Mark.NAUGHT
+
+@dataclass(frozen=True)
+class Grid:
+    cells: str = " " * 9
+
+    def __post_init__(self) -> None:
+        if not re.match(r"^[\sXO]{9}$", self.cells):
+            raise ValueError("Must contain 9 cells of: X, O, or space")
+
+    @cached_property
+    def x_count(self) -> int:
+        return self.cells.count("X")
+
+    @cached_property
+    def o_count(self) -> int:
+        return self.cells.count("O")
+
+    @cached_property
+    def empty_count(self) -> int:
+        return self.cells.count(" ")
